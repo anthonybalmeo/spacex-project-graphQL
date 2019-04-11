45ed7bee1bbb20a769838b06e091e108db484f28
Anthony Balmeo <anthonybalmeo@Anthonys-iMac.local>
2019-04-10 21:59:41 -0700

    Display launch data in ui

--- client/src/components/Launches/LaunchItem/index.js
+++ client/src/components/Launches/LaunchItem/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import classNames from 'classnames'
+import Moment from 'react-moment'
+import { Link } from 'react-router-dom'
 
 export default function LaunchItem(props) {
   const {
     launch: {
       flight_number,
       mission_name,
-      launch_year,
       launch_success,
       launch_date_local,
     },
@@ -15,11 +17,18 @@ export default function LaunchItem(props) {
     <div className='card card-body mb-3'>
       <div className='row'>
         <div className='col-md-9'>
-          <h4>Mission: {mission_name}</h4>
-          <p>Date: {launch_date_local}</p>
+          <h4>Mission:
+            <span className={classNames({
+              'text-success': launch_success,
+              'text-danger': !launch_success,
+           })}>
+            {mission_name}
+            </span>
+          </h4>
+          <p>Date: <Moment format="YYYY-MM-DD HH:mm">{launch_date_local}</Moment></p>
         </div>
         <div className='col-md-3'>
-          <button className='btn btn-secondary'>Launch Details</button>
+          <Link className='btn btn-secondary' to={`/launch/${flight_number}`}>Launch Details</Link>
         </div>
       </div>
     </div>
