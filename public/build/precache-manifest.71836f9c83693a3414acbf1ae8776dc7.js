7914bfe8f9239358a72b190ae38ba622c11ac0b5
Anthony Balmeo <anthonybalmeo@Anthonys-iMac.local>
2019-04-10 22:29:40 -0700

    Update build

---
+++ public/build/precache-manifest.71836f9c83693a3414acbf1ae8776dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "950ba66541181838b6c07f14d62e331a",
+    "url": "/static/media/logo.950ba665.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9e4dd68835066ee43c9f",
+    "url": "/static/js/main.bbbc154d.chunk.js"
+  },
+  {
+    "revision": "4cb19dd541ca30ae2d99",
+    "url": "/static/js/2.d78e3e7b.chunk.js"
+  },
+  {
+    "revision": "9e4dd68835066ee43c9f",
+    "url": "/static/css/main.5f78a665.chunk.css"
+  },
+  {
+    "revision": "0cccef350db8c2f8ebd387f0cb02d1f4",
+    "url": "/index.html"
+  }
+];
